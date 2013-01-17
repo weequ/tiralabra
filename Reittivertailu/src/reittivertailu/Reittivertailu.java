ff4564cf9b93eeb20e8c4b47c5e0212470b5f6c1
Antti <antti.kaik@gmail.com>
2013-01-17 17:56:46 +0200

    Projekti aloitettu. Osa luokista(käyttöliittymä, Reittivertailu) on vielä melko sotkuisia.

--- Reittivertailu/src/reittivertailu/Reittivertailu.java
+++ Reittivertailu/src/reittivertailu/Reittivertailu.java
@@ -4,16 +4,36 @@
  */
 package reittivertailu;
 
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import kayttoliittyma.GUI;
+import ruudukko.Ruudukko;
+import tiedostonLuku.Lukija;
+
 /**
  *
  * @author Antti
  */
 public class Reittivertailu {
 
-    /**
-     * @param args the command line arguments
-     */
+    public Reittivertailu() {
+        Lukija lukija = null;
+        try {
+            lukija = new Lukija("testikartta.txt");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Reittivertailu.class.getName()).log(Level.SEVERE, null, ex);
+            System.exit(0);
+        }
+        try {
+            GUI gui = new GUI(new Ruudukko(lukija.getText()));
+            gui.pack();
+        } catch (Exception ex) {
+            Logger.getLogger(Reittivertailu.class.getName()).log(Level.SEVERE, null, ex);
+            System.exit(0);
+        }
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Reittivertailu();
     }
 }
